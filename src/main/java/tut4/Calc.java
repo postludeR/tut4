2b0867b32543116eaadf1024a86570be957b9465
postludeR <nemopostlude@gmail.com>
2022-09-23 19:43:16 +0800

    travis

--- src/main/java/tut4/Calc.java
+++ src/main/java/tut4/Calc.java
@@ -1,2 +1,11 @@
-package tut4;public class Calc {
+package tut4;
+
+public class Calc {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int substract(int a, int b) {
+        return a + b;
+    }
 }
--- src/test/java/tut4/CalcTest.java
+++ src/test/java/tut4/CalcTest.java
@@ -1,4 +1,14 @@
+package tut4;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CalcTest {
-  
+
+    @Test
+    void add() {
+        Calc test = new Calc();
+        assertEquals(test.add(1,2), 3);
+    }
 }
